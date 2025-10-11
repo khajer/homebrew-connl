52f0a3fafa28f20d7ae886137aaae6fc311c3c2b
itsara <khajer@gmail.com>
2025-10-11 16:30:58 +0700

    remove sha: prefix and use only sha256 code

--- Formula/connl.rb
+++ Formula/connl.rb
@@ -2,7 +2,7 @@ class Connl < Formula
   desc "the client app use to connect the connl server"
   homepage "https://github.com/khajer/bindlocal-client"
   url "https://github.com/khajer/bindlocal-client/releases/download/v0.0.1/connl-0.0.1-macos.tar.gz"
-  sha256 "sha256:ee82527ca72adb4b0b8facdc582e743a4a17d2a2dc380d49059d7a2bf45fdbe3"
+  sha256 "ee82527ca72adb4b0b8facdc582e743a4a17d2a2dc380d49059d7a2bf45fdbe3"
   license "MIT"
 
   def install
