a8fb684eb37e74e7b103360ad59ca7f03902e188
itsara <khajer@gmail.com>
2025-10-12 19:08:15 +0700

    support install connl with brew

--- Formula/connl.rb
+++ Formula/connl.rb
@@ -1,9 +1,18 @@
 class Connl < Formula
   desc "the client app use to connect the connl server"
   homepage "https://github.com/khajer/bindlocal-client"
-  url "https://github.com/khajer/bindlocal-client/releases/download/v0.0.1/connl-0.0.1-macos.tar.gz"
-  sha256 "sha256:ee82527ca72adb4b0b8facdc582e743a4a17d2a2dc380d49059d7a2bf45fdbe3"
+  version "0.0.1"
   license "MIT"
+
+  on_macos do
+    url "https://github.com/khajer/bindlocal-client/releases/download/v0.0.1/connl-0.0.1-macos.tar.gz"
+    sha256 "sha256:ee82527ca72adb4b0b8facdc582e743a4a17d2a2dc380d49059d7a2bf45fdbe3"
+  end
+  on_linux do
+    url "https://github.com/khajer/bindlocal-client/releases/download/v0.0.1/connl-0.0.1-linux.tar.gz"
+    sha256 "sha256:394cb338b9d6b9fd8abcd6399909aad396aad5177d5f88ab9f65922e692e794d"
+  end
+
 
   def install
     bin.install "connl"
